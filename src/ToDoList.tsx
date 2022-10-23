f910f72de478b8ea90b8935f399c5c0d0336354d
Jungwoo Yang <41814535+JungwooYang@users.noreply.github.com>
2022-10-23 20:46:01 +0900

    todos list

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,23 +1,46 @@
 import { useForm } from "react-hook-form";
-
-interface Iform {
+import { useRecoilState, atom } from "recoil";
+interface IForm {
   todo: string;
 }
 
+interface ITodo {
+  id: number;
+  text: string;
+  category: "TODO" | "DOING" | "DONE";
+}
+
+const todoState = atom<ITodo[]>({
+  key: "todo",
+  default: [],
+});
+
 function TodoList() {
-  const { register, handleSubmit, setValue } = useForm<Iform>();
-  const handleValid = (data: Iform) => {
-    console.log(data);
+  const [todos, setTodos] = useRecoilState(todoState);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = ({ todo }: IForm) => {
+    setTodos((newTodos) => [
+      { text: todo, id: Date.now(), category: "TODO" },
+      ...newTodos,
+    ]);
     setValue("todo", "");
   };
   return (
     <div>
+      <h1>TODOS</h1>
+      <hr />
       <form onSubmit={handleSubmit(handleValid)}>
         <input
           {...register("todo", { required: "Please write a todo" })}
           placeholder="Write a todo"
         />
+        <button>ADD</button>
       </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
