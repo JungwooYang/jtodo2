42a6d951a6323939fb1657ed8fb6a5cb44ac9d34
Jungwoo Yang <41814535+JungwooYang@users.noreply.github.com>
2022-10-23 15:13:08 +0900

    todo start

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,88 +1,22 @@
 import { useForm } from "react-hook-form";
 
-/* function TodoList() {
-  const [todo, setTodo] = useState("");
-  const [todoError, setTodoError] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setTodo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if(todo.length < 10) {
-      return setTodoError("Todo should be longer than 10 letters.")
-    }
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={todo}
-          placeholder="what do you have to do?"
-        />
-        <button>add</button>
-      </form>
-    </div>
-  );
-}
-*/
-
 interface Iform {
-  email: string;
-  name: string;
-  password: string;
-  lover: string;
+  todo: string;
 }
 
 function TodoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Iform>({ defaultValues: { email: "@naver.com" } });
-  const onValid = (data: any) => {
+  const { register, handleSubmit, setValue } = useForm<Iform>();
+  const handleValid = (data: Iform) => {
     console.log(data);
+    setValue("todo", "");
   };
   return (
     <div>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
+      <form onSubmit={handleSubmit(handleValid)}>
         <input
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
-              message: "Only naver.com emails available",
-            },
-          })}
-          placeholder="email"
-          style={{ borderColor: errors?.email?.message ? "red" : "" }}
+          {...register("todo", { required: "Please write a todo" })}
+          placeholder="Write a todo"
         />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("name", { required: "write here" })}
-          placeholder="name"
-          style={{ borderColor: errors?.name?.message ? "red" : "" }}
-        />
-        <span>{errors?.name?.message}</span>
-        <input
-          {...register("password", { required: "write here" })}
-          placeholder="password"
-          style={{ borderColor: errors?.password?.message ? "red" : "" }}
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("lover", { required: "write here" })}
-          placeholder="lover"
-          style={{ borderColor: errors?.lover?.message ? "red" : "" }}
-        />
-        <span>{errors?.lover?.message}</span>
-        <button>add</button>
       </form>
     </div>
   );
